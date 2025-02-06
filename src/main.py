fd2d104fc1a88a0eb5550fc6ba4a9999e905da8d
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2025-02-05 22:56:48 -0600

    main.py updated with dummy data and class finalized

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-#hello world
-print("hello world")
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("Stuff and things", TextType.ITALIC, "www.made_up_url.com")
+    print(node)
+
+    print(f"Happy Blank-Day")
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -10,9 +10,9 @@ class TextType(Enum):
 
 class TextNode:
     def __init__(self, text, text_type, url=None):
-    self.text = text
-    self.text_type = text_type
-    self.url = url
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
     def __eq__(self, other):
         if isinstance(other, TextNode): 
