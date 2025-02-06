284c565f2a69fc9a53d8b90b4a9cf9739e041d10
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2025-02-05 22:44:51 -0600

    textnode class wrap up and tidying

--- src/textnode.py
+++ src/textnode.py
@@ -5,17 +5,19 @@ class TextType(Enum):
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
-    LINKS = "links"
-    IMAGES = "images"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url=None)
+    def __init__(self, text, text_type, url=None):
     self.text = text
     self.text_type = text_type
     self.url = url
 
     def __eq__(self, other):
-        if self.text == other.text and self.text_type == other.text_type and self.url == other.url: 
-            return True
-    return False
+        if isinstance(other, TextNode): 
+            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return False
 
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
