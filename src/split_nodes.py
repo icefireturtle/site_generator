156045e2f1b95fdccf0a35f36d5baa1614e2101d
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2025-02-28 23:45:17 -0600

    split images and links created in split nodes and unit tests updated

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from regex import extract_markdown_images, extract_markdown_links
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -9,7 +10,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         split_nodes = []
         section = old_node.text.split(delimiter)
         if len(section) % 2 == 0:
-            raise ValueError("invalide markdown, formatted section not closed")
+            raise ValueError("invalid markdown, formatted section not closed")
         for i in range(len(section)):
             if section[i] == "":
                 continue
@@ -20,3 +21,64 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes    
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+
+        extract = list(extract_markdown_images(old_node.text))
+        
+        if not extract:
+            new_nodes.append(old_node)
+            continue
+        
+        text = old_node.text
+
+        for image_alt, image_link in extract:
+            section = text.split(f"![{image_alt}]({image_link})", 1)
+            
+            if section[0]:
+                new_nodes.append(TextNode(section[0], old_node.text_type))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            if len(section) > 1:
+                text = section[1]
+            else:
+                text = ""
+        
+        if text:
+            new_nodes.append(TextNode(text, old_node.text_type))
+
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+
+        extract = list(extract_markdown_links(old_node.text))
+        
+        if not extract:
+            new_nodes.append(old_node)
+            continue
+        
+        text = old_node.text
+
+        for link_text, link_url in extract:
+            section = text.split(f"[{link_text}]({link_url})", 1)
+            
+            if section[0]:
+                new_nodes.append(TextNode(section[0], old_node.text_type))
+
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+
+            if len(section) > 1:
+                text = section[1]
+            else:
+                text = ""
+        
+        if text:
+            new_nodes.append(TextNode(text, old_node.text_type))
+
+    return new_nodes
--- src/test_splitnodes.py
+++ src/test_splitnodes.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from split_nodes import split_nodes_delimiter
+from split_nodes import split_nodes_delimiter, split_nodes_image, split_nodes_link
 
 class TestSplitNode(unittest.TestCase):
     def test_delim_bold(self):
@@ -72,6 +72,38 @@ class TestSplitNode(unittest.TestCase):
             ],
             new_nodes,
         )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is markdown text including an image and url with alt ![image](https://imgur.com/zZZZZZkqd.png) and another ![second image](https://imgur.com/elkZZZZZZzzkqc.png)",
+            TextType.NORMAL,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is markdown text including an image and url with alt ", TextType.NORMAL),
+                TextNode("image", TextType.IMAGE, "https://imgur.com/zZZZZZkqd.png"),
+                TextNode(" and another ", TextType.NORMAL),
+                TextNode("second image", TextType.IMAGE, "https://imgur.com/elkZZZZZZzzkqc.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "Here is text with stinky links [google](https://google.com) and more googs [second google](https://google.com)", TextType.NORMAL,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("Here is text with stinky links ", TextType.NORMAL),
+                TextNode("google", TextType.LINK, "https://google.com"),
+                TextNode(" and more googs ", TextType.NORMAL),
+                TextNode("second google", TextType.LINK, "https://google.com"),
+            ],
+            new_nodes,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
