447a9bf739f3147b2a1eb8ff734009d533e0d590
icefireturtle <194567854+icefireturtle@users.noreply.github.com>
2026-03-09 17:53:53 -0500

    created makrdown_to_html file and defined helper function text_to_children that parses text nodes and adds them to children list as well as markdown_to_html function that determines block type and matches the case appends the block to a list and then returns a parent div

---
+++ markdown_to_html.py
@@ -0,0 +1,77 @@
+from blocktype import BlockType, block_to_block_type
+from split_nodes import markdown_to_blocks
+from htmlnode import HTMLNode
+from parentnode import ParentNode
+from textnode_to_htmlnode import text_node_to_html_node
+from split_nodes import text_to_textnodes
+from leafnode import LeafNode
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def markdown_to_html(md):
+    blocks = markdown_to_blocks(md)
+    block_list = []
+    parent_div = ParentNode("div", block_list)
+
+    for block in blocks:
+        type = block_to_block_type(block)
+        
+        
+        match type:
+            case "paragraph":
+                cleaned_text = " ".join(block.split())
+                children = text_to_children(cleaned_text)
+                node = ParentNode("p", children)
+                block_list.append(node)
+            case "heading":
+                heading_number = 0
+                for char in block:
+                    if char == "#":
+                        heading_number += 1
+                    else:
+                        break
+                text = block[heading_number + 1:]
+                children = text_to_children(text)
+                node = ParentNode(f"h{heading_number}", children)
+                block_list.append(node)
+            case "code":
+                code_content = block.strip("`").strip("\n")
+                code_node = LeafNode("code", code_content)
+                node = ParentNode("pre", [code_node])
+                block_list.append(node)
+            case "quote":
+                lines = block.split("\n")
+                stripped_lines = []
+                for line in lines:
+                    if not line.startswith(">"):
+                        raise ValueError("invalid quote block")
+                    stripped_lines.append(line.lstrip(">").strip())
+                cleaned_text = " ".join(stripped_lines)
+                children = text_to_children(cleaned_text)
+                node = ParentNode("blockquote", children)
+                block_list.append(node)
+            case "unordered_list":
+                lines = block.split("\n")
+                li_lines = []
+                for line in lines:
+                    line_content = line[2:]
+                    li_lines.append(ParentNode("li", text_to_children(line_content)))
+                node = ParentNode("ul", li_lines)
+                block_list.append(node)
+            case "ordered_list":
+                lines = block.split("\n")
+                li_lines = []
+                for line in lines:
+                    space_index = line.find(" ")
+                    line_content = line[space_index + 1:]
+                    li_lines.append(ParentNode("li", text_to_children(line_content)))
+                node = ParentNode("ol", li_lines)
+                block_list.append(node)
+
+    return parent_div
